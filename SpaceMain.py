c263cc4d02f71cc11ebdbfedab040a5951d72377
Baptiste <baptiste.boiteux@cpe.fr>
2020-12-18 11:24:11 +0100

    mouvement alien

--- SpaceMain.py
+++ SpaceMain.py
@@ -10,27 +10,47 @@ repertoire git : https://github.com/BaptisteBoiteux/SpaceInvader.git
 import SpaceFonction as f
 from tkinter import Tk, Label, Button, Canvas, Entry, StringVar,messagebox, PhotoImage,filedialog, Menu
 
-class vaisseau:
-    def __init__(self):
-        self.Zone_jeux.create
+
+largeur = 480
+hauteur = 320
+x = 20
+moitie_alien = 20
+dx = 2
+
+def deplacement_alien():
+    """ Deplacement de l'alien"""
+    global x,dx,largeur,hauteur
+    # rebond à droite
+    if x+moitie_alien+ dx > largeur:
+        x = 2*(largeur-moitie_alien)-x
+        dx = -dx
+        
+    # rebond à gauche
+    if x-moitie_alien+dx < 0:
+        x = 2*moitie_alien-x
+        dx = -dx
+    x = x+dx
+    # affichage
+    Zone_jeux.coords(alien,x-moitie_alien,10,x+moitie_alien,50)
+    # mise à jour toutes les 200ms
+    mw.after(50,deplacement_alien)
 
 # création de la fenêtre graphique
 mw = Tk()
 score = StringVar()
 score.set("score:0")
-mw.title('Space Invader')
+mw.title('Bretons Invader')
 # Création d'un widget Canvas (zone graphique)
-Largeur = 480
-Hauteur = 320
-Zone_jeux = Canvas(mw, width = Largeur, height =Hauteur, bg ='grey')
+Zone_jeux = Canvas(mw, width = largeur, height = hauteur, bg ='grey')
 Zone_jeux.pack(side = 'top',padx =5, pady =5)
+alien = Zone_jeux.create_rectangle(x-moitie_alien,10,x+moitie_alien,50)
 # Création d'un widget Label (score)
 Label1 = Label(mw,textvariable = score)
 Label1.pack(side = 'bottom', padx = 5, pady = 5)
 # Création d'un widget Button (bouton Quitter)
 Button(mw, text ='Quitter' ,command = mw.destroy).pack(side='bottom',padx=5,pady=5)
 # Création d'un widget Button (bouton Commencer)
-Button(mw, text ='Commencer', command = f.Debut ).pack(side='bottom',padx=5,pady=5)
+Button(mw, text ='Commencer', command = deplacement_alien ).pack(side='bottom',padx=5,pady=5)
 # Création d'un widget Menu
 menubar = Menu(mw)
 menuoptions = Menu(menubar,tearoff = 0)
