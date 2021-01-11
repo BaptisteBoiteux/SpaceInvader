f25554221f2459ea2cee9f891f3dfb95830219e2
Julien <jm62140@gmail.com>
2021-01-11 23:04:56 +0100

    graphique separer

--- SpaceFonction.py
+++ SpaceFonction.py
@@ -25,3 +25,46 @@ class Alien():
         self.rebond = 0
         self.dx = 5
         self.detruit = False
+
+class Vaisseau():
+    def __init__(self):
+        self.x  = 240
+        self.x0 = 220
+        self.x1 = 260
+        self.y  = 280
+        self.y1 = 302
+        self.y0 = 258
+    def droite(self):
+        self.x = self.x + 10
+        self.x1 = self.x1 + 10
+        self.x0 = self.x0 + 10  
+    def gauche(self):       
+        self.x  = self.x - 10
+        self.x1 = self.x1 - 10
+        self.x0 = self.x0 - 10  
+
+
+class Missile():
+    def __init__(self,master,x0,y0,sens):
+        self.master = master
+        self.sens = sens
+        self.img = PhotoImage(file="Image/tha_le_misille.png")
+        if self.sens == 'vaisseau':
+            self.x0 = x0
+            self.x1 = x0+40
+            self.y0 = y0-40
+            self.y1 = y0
+            self.misille = self.master.create_image(self.x1-20,self.y1-20,image = self.img)
+        if self.sens == 'alien':
+            self.x0 = x0+20
+            self.x1 = x0+40
+            self.y0 = y0+40
+            self.y1 = y0
+            self.misille = self.master.create_image(self.x1-20,self.y1-20,image = self.img)
+    def deplacement_missile(self):
+            if self.sens == 'vaisseau':
+                if self.x0 > 10:
+                    self.master.move(self.misille,0,-10)
+                    self.x1 = self.y1 - 10
+                    self.x0 = self.y0 - 10  
+                    self.master.after(1000,self.deplacement_missile())
--- SpaceMain.py
+++ SpaceMain.py
@@ -13,29 +13,24 @@ from tkinter import Tk, Label, Button, Canvas, Entry, StringVar,messagebox, Phot
 
 
 
-class Vaisseau():
-    def __init__(self,master):
-        self.master = master
-        self.img = PhotoImage(file="Image/Logo_RogerVoyage1.png")
-        self.vaisseaux = self.master.create_image(240, 280,image=self.img)
-        self.x0 = 220
-        self.x1 = 260
-        self.y1 = 302
-        self.y0 = 258
-    def droite(self):
-        if (self.x1 <= 470):
-            self.master.move(self.vaisseaux,10,0)
-            self.x1 = self.x1 + 10
-            self.x0 = self.x0 + 10  
-    def gauche(self):
-        if (self.x0 >= 10):
-            self.master.move(self.vaisseaux,-10,0)
-            self.x1 = self.x1 - 10
-            self.x0 = self.x0 - 10  
+#def bigLoop():  #boucle de jeu
+#    instancier ton missile
+#    missile = ...
+#    dessin_missile
+#
+#  vaisseau =
+#   dessin_vaisseau
+#
+#    ennemis =
+#    dessin_ennemisx    
+#    
+#    Toutes les 50ms, rappeler bigLoop()
+
 
 
 alien0 = f.Alien(60,20,30,30)
 alien1 = f.Alien(60,20,50,30)
+roger = f.Vaisseau()
 largeur_mw = 480
 hauteur_mw = 320
 play = False
@@ -44,34 +39,49 @@ def Commencer():
     """Commande qui se lance a l'appui du bouton commencer"""
     global play
     if not play :
-        deplacement_alien(alien0)
+        bigloop(roger)
     play = True
 
+def bigloop (roger): 
+    print(roger.x,roger.y)
+    mw.after(50,lambda:bigloop(roger))
 
-def deplacement_alien(alien):
-    """ Deplacement de l'alien"""
-    moitie_x = alien.largeur/2
-    moitie_y = alien.hauteur/2
-    # rebond à droite
-    if alien.x+moitie_x+ alien.dx > largeur_mw:
-        alien.x = 2*(largeur_mw-moitie_x)-alien.x
-        alien.dx = -alien.dx
-        alien.rebond += 1
-    # rebond à gauche
-    if alien.x-moitie_x+ alien.dx < 0:
-        alien.x = 2*moitie_x-alien.x
-        alien.dx = -alien.dx
-        alien.rebond +=1
-    alien.x += alien.dx
-    #descente de l'alien
-    if alien.rebond == 2:
-        alien.y += 10
-        alien.rebond = 0
-    # affichage
-    Zone_jeux.coords(alien_rec,alien.x-moitie_x,alien.y-moitie_y,alien.x+moitie_x,alien.y+moitie_y)
-    # mise à jour toutes les 50ms
-    mw.after(50,lambda:deplacement_alien(alien))
+def droite():
+    if (roger.x1 <= 470):
+        print("droite")
+        roger.droite()
+        Zone_jeux.move(roger_vaisseau,10,0)
 
+def gauche():
+    if (roger.x0 >= 10):
+        print("gauche")
+        roger.gauche()
+        Zone_jeux.move(roger_vaisseau,-10,0)
+
+# def deplacement_alien(alien):
+#     """Deplacement de l'alien"""
+#     moitie_x = alien.largeur/2
+#     moitie_y = alien.hauteur/2
+#     # rebond à droite
+#     if alien.x+moitie_x+ alien.dx > largeur_mw:
+#         alien.x = 2*(largeur_mw-moitie_x)-alien.x
+#         alien.dx = -alien.dx
+#         alien.rebond += 1
+#     # rebond à gauche
+#     if alien.x-moitie_x+ alien.dx < 0:
+#         alien.x = 2*moitie_x-alien.x
+#         alien.dx = -alien.dx
+#         alien.rebond +=1
+#     alien.x += alien.dx
+#     #descente de l'alien
+#     if alien.rebond == 2:
+#         alien.y += 10
+#         alien.rebond = 0
+#     # affichage
+#     Zone_jeux.coords(alien_rec,alien.x-moitie_x,alien.y-moitie_y,alien.x+moitie_x,alien.y+moitie_y)
+#     # mise à jour toutes les 50ms
+#     mw.after(50,lambda:deplacement_alien(alien))
+    
 # création de la fenêtre graphique
 mw = Tk()
 score = StringVar()
@@ -81,6 +91,8 @@ mw.title('Bretons Invader')
 Zone_jeux = Canvas(mw, width = largeur_mw, height = hauteur_mw, bg ='grey')
 Zone_jeux.pack(side = 'top',padx =5, pady =5)
 alien_rec = Zone_jeux.create_rectangle(alien0.x-alien0.largeur/2,alien0.y-alien0.hauteur/2,alien0.x+alien0.largeur/2,alien0.y+alien0.hauteur/2)
+img_vaisseau = PhotoImage(file='Image/Logo_RogerVoyage1.png')
+roger_vaisseau = Zone_jeux.create_image(roger.x,roger.y,image= img_vaisseau)
 #alien1 = Zone_jeux.create_rectangle(alien1.x-moitie_x,y-moitie_y,x+moitie_x,y+moitie_y)
 # Création d'un widget Label (score)
 Label1 = Label(mw,textvariable = score)
@@ -100,10 +112,11 @@ menubar.add_cascade(label = "Aide", menu = menuaide)
 # Affichage du menu
 mw.config(menu = menubar)
 #vaisseaux
-roger = Vaisseau(Zone_jeux)
+mw.bind('<Right>', lambda _:droite())
+mw.bind('<Left>', lambda _:gauche())
 #detection des input
-mw.bind('<Right>', lambda _:roger.droite())
-mw.bind('<Left>', lambda _:roger.gauche())
+# mw.bind('<Right>', lambda _:roger.droite())
+# mw.bind('<Left>', lambda _:roger.gauche())
 
 #lancement du gestionnaire d'événements
 mw.mainloop()
